7c2b33ec7eceab7beb3efe1ff841fb3a899638d9
Samsung <Samsung>
2016-06-13 17:33:09 -0300

    Adicionado templates

---
+++ module/Gerador/src/Gerador/Controller/GeradorController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Gerador\Controller;
+
+use Estrutura\Controller\AbstractEstruturaController;
+use Gerador\Service\Gerador;
+use Gerador\Service\GeradorColuna;
+use Gerador\Service\GeradorTabela;
+
+class GeradorController extends AbstractEstruturaController {
+
+    protected function playAction() {
+
+        $id = $this->params('id');
+
+        $service = new GeradorTabela();
+        $tabelas = $service->filtrarObjeto();
+
+        $colunas = new GeradorColuna();
+        $mapa = [];
+        foreach ($tabelas as $tabela) {
+
+            if ($id) {
+                if ($id === $tabela->getTableName()) {
+                    
+                    $colunas->setTableSchema($tabela->getTableSchema());
+                    $colunas->setTableName($tabela->getTableName());                    
+                    $mapa[$tabela->getTableName()] = $colunas->filtrarObjeto();
+                }
+            } else {
+                $colunas->setTableName($tabela->getTableName());
+                $colunas->setTableName($tabela->getTableName());                    
+                $mapa[$tabela->getTableName()] = $colunas->filtrarObjeto();
+            }
+        }
+        
+        $gerador = new Gerador($mapa);
+        $gerador->gerar();
+
+        $this->addSuccessMessage('Crud gerado com sucesso. Habilite o módulo "' . $id . '" no \config\application.config.php');
+
+        return $this->redirect()->toRoute('gerador-home');
+    }
+
+}
